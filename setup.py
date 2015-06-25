180756814e2df4abf8519eb4d3647b963c75b929
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-06-25 16:19:08 +0100

    Added version number

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
+from pytest_mpl import __version__
+
 setup(
+    version=__version__,
     name="pytest-mpl",
     packages = ['pytest_mpl'],
     entry_points = {'pytest11': ['pytest_mpl = pytest_mpl.plugin',]},
