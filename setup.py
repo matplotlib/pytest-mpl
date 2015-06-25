21e2ab79c21d621d9a204fa2c674a221ea69130e
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-06-25 16:49:20 +0100

    Added information to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ from pytest_mpl import __version__
 setup(
     version=__version__,
     name="pytest-mpl",
+    description='pytest plugin to help with testing figures output from Matplotlib',
     packages = ['pytest_mpl'],
+    license='BSD',
+    author='Thomas Robitaille',
+    author_email='thomas.robitaille@gmail.com',
     entry_points = {'pytest11': ['pytest_mpl = pytest_mpl.plugin',]},
 )
