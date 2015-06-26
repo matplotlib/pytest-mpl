b6a86cf9fa6f60ceedb17ab8493b7cba6b2f029b
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-06-26 10:39:23 +0100

    Convert README.md to rst for PyPI

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@ from setuptools import setup
 
 from pytest_mpl import __version__
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    with open('README.md') as infile:
+        long_description = infile.read()
+
 setup(
     version=__version__,
     name="pytest-mpl",
     description='pytest plugin to help with testing figures output from Matplotlib',
+    long_description=long_description,
     packages = ['pytest_mpl'],
     license='BSD',
     author='Thomas Robitaille',
