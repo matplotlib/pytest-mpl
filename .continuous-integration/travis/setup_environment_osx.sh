e6b32967b06601964f5f1a4103ce531e84befb4e
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-02-02 13:01:43 +0000

    Updated .travis.yml and appveyor.yml to use ci-helpers

--- .continuous-integration/travis/setup_environment_osx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install conda
-wget http://repo.continuum.io/miniconda/Miniconda-3.7.3-MacOSX-x86_64.sh -O miniconda.sh
-chmod +x miniconda.sh
-./miniconda.sh -b
-export PATH=/Users/travis/miniconda/bin:$PATH
-conda update --yes conda
