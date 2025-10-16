79dbd43d9394f1d63bb0e515c8dcc5a616e064f4
Conor MacBride <conor@macbride.me>
2025-10-16 01:13:31 +0100

    Revert "run pytester subprocess when matplotlib built with pybind11 v3"
    This reverts commit b66e2a991de3991097b4516b53975008e895b3e1.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,12 +7,3 @@ if Version(pytest.__version__) < Version("6.2.0"):
     @pytest.fixture
     def pytester(testdir):
         return testdir
-
-
-def pytest_configure(config):
-    # Matplotlib versions build with pybind11 3.0.0 or later
-    # encounter import issues unless run in subprocess mode.
-    # See: https://github.com/matplotlib/pytest-mpl/issues/248
-    import matplotlib
-    if Version(matplotlib.__version__) > Version("3.10.3"):
-        config.option.runpytest = "subprocess"
